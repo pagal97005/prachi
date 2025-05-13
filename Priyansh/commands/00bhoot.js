cf87a083815f34bf22931250861fe66c47ba9013
Kriti970 <kritikori209@gmail.com>
2025-05-13 07:09:55 +0530

    Update 00bhoot.js

--- Priyansh/commands/00bhoot.js
+++ Priyansh/commands/00bhoot.js
@@ -22,6 +22,6 @@ const fs = global.nodemodule["fs-extra"];
     var link = [
       "https://i.imgur.com/O3w9rfy.jpeg","https://i.imgur.com/Qdmp3ur.jpeg","https://i.imgur.com/TUCL77W.jpeg","https://i.imgur.com/aW0yjtL.jpeg","https://i.imgur.com/qlk2hzU.jpeg","https://i.imgur.com/zZ369xs.jpeg","https://i.imgur.com/Z7NcgkG.jpeg","https://i.imgur.com/E3msrCy.jpeg","https://i.imgur.com/FB4L7S9.jpeg","https://i.imgur.com/IfMqV9z.jpeg","https://i.imgur.com/9fhNfiS.jpeg" 
     ];
-         var callback = () => api.sendMessage({body:`╔══❀═══◄░❀ βΉΦΦτ ░►═══❀══╗•`,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
+         var callback = () => api.sendMessage({body:` βΉΦΦτ `,attachment: fs.createReadStream(__dirname + "/cache/1.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));  
           return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/1.jpg")).on("close",() => callback());
        };
