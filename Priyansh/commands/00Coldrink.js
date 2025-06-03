f27edc7829264c3e9969a9486c6ac685273a3650
Kriti970 <kritikori209@gmail.com>
2025-06-03 17:38:18 +0530

    Create 00Coldrink.js

---
+++ Priyansh/commands/00Coldrink.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "coldrink",
+    version: "1.1.1",
+  hasPermssion: 0,
+  credits: "PREM BABU", 
+  description: "Just Respond",
+  usePrefix: true,
+  commandCategory: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+  var { threadID, messageID } = event;
+  let react = event.body.toLowerCase();
+  if(react.includes("COLDDRINK") ||
+     react.includes("colddrink") || react.includes("ColDrInk") || react.includes("PePsi") ||
+react.includes("coldrink") ||
+react.includes("pepsi")) {
+    var msg = {
+        body: `😍TUM ISHI LAYAK HO 😍`,attachment: fs.createReadStream(__dirname + `/noprefix/coldrink.jpg`)
+      }
+      api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🥤", event.messageID, (err) => {}, true)
+    }
+  }
+  module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
